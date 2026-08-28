cb671c48c2ecdea80fd0c088b6813d83f27bd843
firza pradipta atalla <pradipta.firza@gmail.com>
2026-08-28 14:53:22 +0700

    Rewrote stg_order_items.sql with a CTE approach for better readability.

--- looker_basket/models/staging/stg_order_items.sql
+++ looker_basket/models/staging/stg_order_items.sql
@@ -1,7 +1,21 @@
+WITH source AS (
+    SELECT
+        *
+    FROM 
+        {{ source('thelook_ecommerce', 'order_items') }}
+),
+
+orders AS (
+    SELECT
+        id AS order_item_id,
+        order_id,
+        product_id,
+        sale_price
+    FROM
+        source
+)
+
 SELECT
-    id AS order_item_id,
-    order_id,
-    product_id,
-    sale_price
+    *
 FROM 
-    {{ source('thelook_ecommerce', 'order_items') }}
+    orders

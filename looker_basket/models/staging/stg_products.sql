0c58b60fd65de0ea12e24ea76f704ca1600d6329
firza pradipta atalla <pradipta.firza@gmail.com>
2026-08-28 14:50:33 +0700

    Rewrote stg_products.sql with a CTE approach for better readability.

--- looker_basket/models/staging/stg_products.sql
+++ looker_basket/models/staging/stg_products.sql
@@ -1,7 +1,21 @@
-SELECT 
-    id AS product_id,
-    COALESCE(name, 'Unknown Product') as product_name,
-    category,
-    department
+WITH source AS (
+    SELECT
+        *
+    FROM
+        {{ source('thelook_ecommerce', 'products') }}
+),
+
+products AS (
+    SELECT
+        id AS product_id,
+        COALESCE(name, 'Unknown Product') AS product_name,
+        category,
+        department
+    FROM
+        source
+)
+
+SELECT
+    *
 FROM
-    {{ source('thelook_ecommerce', 'products') }}
+    products

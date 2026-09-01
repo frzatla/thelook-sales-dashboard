ae657d3efd99ba0320f9d66f83e7be0b763d2100
firza pradipta atalla <pradipta.firza@gmail.com>
2026-09-01 20:42:42 +0700

    Deleted every model related to the previous project objective of making a market basket analysis.

--- looker_basket/models/intermediate/int_total_orders.sql
+++
@@ -1,4 +0,0 @@
-SELECT
-    COUNT(DISTINCT order_id) as total_orders
-FROM
-    {{ ref('stg_order_items') }}
--- looker_basket/models/marts/fct_baskets.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    order_id,
-    total_items,
-    basket_value
-FROM 
-    {{ ref('int_basket_summary') }}
